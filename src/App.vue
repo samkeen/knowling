046f0501a9d43590491880ea250b78608b8cc61b
Sam Keen <sam.sjk@gmail.com>
2024-03-07 19:29:14 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Editor from "./components/Editor.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Notes.ai</h1>
+    <Editor />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
