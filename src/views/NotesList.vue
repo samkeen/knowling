ce6761cd0b93acc3860aee4294a1996fb8f4c926
Sam Keen <sam.sjk@gmail.com>
2024-03-30 15:30:07 -0700

    clean up the Notes list

--- src/views/NotesList.vue
+++ src/views/NotesList.vue
@@ -2,14 +2,11 @@
   <div class="mb-8">
     <h2 class="text-2xl font-bold mb-4">Notes</h2>
     <ul>
-
-      <li v-for="note in notes" :key="note.id" class="mb-4 p-4 bg-white shadow">
+      <li v-for="note in notes" :key="note.id" class="mb-4 p-4 bg-white">
         <RouterLink :to="{ name: 'EditNote', params: { id: note.id } }">
-          <h3 class="font-bold">{{ note.id }}</h3>
-          <p>{{ note.text }}</p>
+          <p class="truncate ...">{{ noteTitle(note.text) }}</p>
         </RouterLink>
       </li>
-
     </ul>
   </div>
 </template>
@@ -30,7 +27,13 @@ async function get_notes() {
     console.error("Failed getting notes:", error);
     // Handle the error as needed, e.g., show a user-friendly message
   }
+}
 
+function noteTitle(text) {
+  const lines = text.split('\n');
+  const firstLine = lines[0];
+  // remove any leading '#' or spaces
+  return firstLine.replace(/^#+\s*/, '');
 }
 
 onMounted(get_notes);
